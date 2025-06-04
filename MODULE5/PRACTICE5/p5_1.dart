9646428cf1c2c2c6fe489da41a89e871ee78eaab
Md. An Nahian Prince <annahian44@gmail.com>
2025-06-04 13:32:05 +0600

    Create p5_1.dart

---
+++ MODULE5/PRACTICE5/p5_1.dart
@@ -0,0 +1,24 @@
+/*
+* Module 4: F-1 Flutter core features
+* */
+
+/*
+* Open source
+* Cross-platform [android, ios, windows, linux etc.]
+* Hot Reload [Real time update to real time app update also]
+* Accessible Native Features and SDKs
+* [Native feature a mobile: Bluetooth, Camera, WiFi, Speaker etc.]
+* Minimal Code[Short code to access them]
+* Widgets[To add anything]
+* */
+
+// void main(){
+//   //create object
+//   var sonObj = new Son();
+//   sonObj.baperTaka();
+//   //access baper taka by uttoridhikar sutre
+//
+//   //bap nijeo tar taka access korte pare
+//   var fatherObj = new Father();
+//   fatherObj.baperTaka();
+// }
