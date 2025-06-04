0ce912b418c033b1b018adffb4f1f5ca1f6224b2
Md. An Nahian Prince <annahian44@gmail.com>
2025-06-04 13:40:25 +0600

    Create ProjectSturcture.dart

---
+++ project_structure/lib/ProjectSturcture.dart
@@ -0,0 +1,72 @@
+
+
+void main(){
+
+  /*
+  .idea
+
+  1. This folder is at the very top of the project structure
+  2. Holds the configuration for Android Studio
+  3. It doesn't matter because we are not going to work with
+  4. So that the content of this folder can be ignored
+
+  .android
+  1. This folder holds a complete Android project
+  2. Used when you build the Flutter application for Android
+  3. When the Flutter code is compiled into the native code
+  4. It will get injected into this Android Project
+
+  .ios
+  1. This folder holds a complete Mac project
+  2. used when you build the Flutter Application for iOS
+  3. When the Flutter code is compiled into the native code
+  4. It will get injected into this iOS project
+  5. that the result is a native iOS application
+  6. Building a Flutter application for iOS is only possible when you
+  are working on macOS
+
+  lib(Library)
+  1. It is an essential folder, which stands for the library
+  2. It is a folder where we will do our 99% of project work
+  3. Inside the lib folder, we will find the Dart files which contain the code
+  of our Flutter application
+  4. By default, this folder contains the file "main.dart", which is the
+  entry file of the Flutter Application
+
+  test
+  1. This folder contains a Dart Code
+  2. Which is written for the Flutter Apllication
+  3. Perform the automated test when building the app
+  4. It won't be too important for us here
+
+  .gitignore
+  1. It is a text file containing a list of files, file extensions, and folders
+  2. that tells Git which files should be ignored in a project
+  3. Git is a version-control file for tracking changes in source
+
+  .metadata
+  1. It is an auto0generated file by the flutter tools
+  2. Used to track the properties of the Flutter project
+  3. This file performs the internal tasks
+  4. So, you do not need to edit the content manually at any time
+
+  pubsec.yaml
+  1. It is the project's configuration file that will use a lot during
+  working with the Flutter Project. It allows you how your application works.
+  This file Contains:
+  * Project general settings such as name
+  * Description, and version of the project
+  * project dependencies
+  * Project assets(e.g., images).
+
+  pubspec.yaml
+  1. It is an auto generated file based on the .yaml file
+  2. It holds more detail setup about all dependencies
+
+
+Summary:
+***lib***
+**pubspec.yaml**0
+
+   */
+}
