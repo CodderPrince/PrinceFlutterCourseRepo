11566ae3d2c12f63c604858b293a33bb7c585e2e
Md. An Nahian Prince <annahian44@gmail.com>
2025-06-04 13:29:09 +0600

    Create common_imports.dart

---
+++ CLASS/lib/common_imports.dart
@@ -0,0 +1,10 @@
+// common_imports.dart
+export 'dart:async'; // For asynchronous programming (Futures, Streams, etc.).
+export 'dart:collection'; // For specialized collections like HashMap, Queue, etc.
+export 'dart:convert'; // For JSON encoding/decoding.
+export 'dart:core'; // This is always included in every Dart program by default.
+export 'dart:io'; // For file I/O, HTTP requests, and interacting with the operating system.
+export 'dart:math'; // For mathematical functions and constants.
+export 'dart:typed_data'; // For working with typed data (Uint8List, etc.).
+
+export 'package:test/test.dart'; // For unit testing.
